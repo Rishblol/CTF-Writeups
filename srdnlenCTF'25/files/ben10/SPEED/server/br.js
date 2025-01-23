59573fd2c36f77682b173255ebed6015e190cb1b
Rishabh Lal <rish90093@gmail.com>
2025-01-23 21:11:04 +0530

    Delete srdnlenCTF'25/files/ben10/SPEED directory

--- srdnlenCTF'25/files/ben10/SPEED/server/br.js
+++
@@ -1,8 +0,0 @@
-const generateDiscountCode = () => {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-    let discountCode = '';
-    for (let i = 0; i < 12; i++) {
-        discountCode += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return discountCode;
-};
